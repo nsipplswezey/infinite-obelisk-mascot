80489d4f5ecadcf1b8cddbf3283a620a5ff9d6ee
Nick S <nsipplswezey@gmail.com>
2015-05-30 00:32:18 -0700

    Clean up dependencies. Reduced add on scroll to 8

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var app = angular.module('app',['infinite-scroll']);
 
 app.controller('DemoController', function($scope){
+
+  //init images function creates and returns an array of integers
   var initImages = function(numberOfImages){
     var result = [];
     for (var i = 1; i < numberOfImages; i++){
@@ -11,9 +13,12 @@ app.controller('DemoController', function($scope){
 
   $scope.images = initImages(8);
 
+
+  //Load more, sets last to be the last integer of $scope.images
+  //And then it creates 50 new images and pushes them to the array
   $scope.loadMore = function(){
     var last = $scope.images[$scope.images.length - 1];
-    for(var i = 1; i <= 50; i++){
+    for(var i = 1; i <= 8; i++){
       $scope.images.push(last+i)
     }
   };
